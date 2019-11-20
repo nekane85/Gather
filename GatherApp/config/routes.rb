0a121c014b6c1d49e289d68ec756748a09bdb745
Nick K <nicholas.e.kane@gmail.com>
2019-11-20 16:49:40 +0000

    Linked_friends_with_route_change

--- GatherApp/config/routes.rb
+++ GatherApp/config/routes.rb
@@ -6,6 +6,14 @@ Rails.application.routes.draw do
   
   root to: "statics#home"
   
+  get 'friends', to: 'statics#friends'
+  
+  # get 'buddy', to: 'statics#buddy'
+  # get 'chat', to: 'statics#chat'
+  # get 'tasks', to: 'statics#tasks'
+  # get 'map', to: 'statics#map'
+  # get 'events', to: 'statics#events'
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
